3677e7cd6e76ee89a74be0756aa13571d15daf40
axvg <axvg@users.noreply.github.com>
2025-05-02 21:39:55 -0500

    feat: update imports, texts printed

--- src/guardian/cli.py
+++ src/guardian/cli.py
@@ -1,7 +1,8 @@
 import typer
-from object_scanner import read_loose, read_packfile
 from pathlib import Path
-from utils import get_git_dir, find_loose_object_dirs, find_packfiles
+
+from guardian.object_scanner import read_loose, read_packfile
+from guardian.utils import get_git_dir, find_loose_object_dirs, find_packfiles
 
 app = typer.Typer()
 
@@ -16,35 +17,31 @@ def scan(repo_path: str):
     if not git_repo_path:
         typer.echo(f"Path {repo_path} is not a git repository!")
         raise typer.Exit(code=2)
-
     loose_dirs = find_loose_object_dirs(git_repo_path)
     pack_dirs = find_packfiles(git_repo_path)
-
     typer.secho(
         f"Tenemos {len(loose_dirs)} loose objects y {len(pack_dirs)} packs",
         fg=typer.colors.BLUE,
         bold=True,
-        )
-
+    )
     if loose_dirs:
         try:
             for obj_dir in loose_dirs:
                 obj = read_loose(obj_dir)
                 typer.secho(
-                    f"o: tipo={obj.obj_type}, sha={obj.sha}, size={obj.size}",
+                    f"o: t={obj.obj_type}, sha={obj.sha}, size={obj.size}",
                     fg=typer.colors.BRIGHT_RED,
                     bold=True,
-                    )
+                )
         except Exception as e:
             typer.echo(f"err en obj loose: {e}")
-
     if pack_dirs:
         try:
             for pack_dir in pack_dirs:
                 pack = read_packfile(pack_dir)
                 for obj in pack:
                     typer.secho(
-                        f"pack: tipo={obj.obj_type}, size={obj.size}",
+                        f"p: t={obj.obj_type}, sha={obj.sha}, size={obj.size}",
                         fg=typer.colors.YELLOW,
                         bold=True,
                         )
